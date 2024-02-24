7adcd92513ad6791289977c4bfc37aa5dd13e6cf
mogulla3 <hs.iarn3@gmail.com>
2024-02-24 14:04:50 +0900

    Strict relative path acquisition.
    expand("%") は常に相対パスを返すとは限らなかった.
    例えば nvim-tree.lua を使ってファイルエクスプローラーから
    ファイルを開いた場合に絶対パスが返ってくる
    
    expand("%:.") もしくは fnamemodify(expand("%"), ":.") とすると正確に取得できる

--- plugin/copy-file-path.lua
+++ plugin/copy-file-path.lua
@@ -1,18 +1,26 @@
+---@param mods string filename-modifiers
+---@return string
+---see: https://vim-jp.org/vimdoc-ja/cmdline.html#filename-modifiers
+local function get_path(mods)
+  return vim.fn.fnamemodify(vim.fn.expand("%"), mods)
+end
+
+---@param path string
 local function copy_to_clipboard(path)
   vim.fn.setreg("+", path)
   vim.api.nvim_echo({ { "Copied: " .. path } }, false, {})
 end
 
 vim.api.nvim_create_user_command("CopyRelativeFilePath", function()
-  copy_to_clipboard(vim.fn.expand("%"))
+  copy_to_clipboard(get_path(":."))
 end, { nargs = 0, force = true, desc = "Copy relative file path to the clipboard" })
 
 vim.api.nvim_create_user_command("CopyAbsoluteFilePath", function()
-  copy_to_clipboard(vim.fn.expand("%:p"))
+  copy_to_clipboard(get_path(":p"))
 end, { nargs = 0, force = true, desc = "Copy absolute file path to the clipboard" })
 
 vim.api.nvim_create_user_command("CopyFileName", function()
-  copy_to_clipboard(vim.fn.expand("%:t"))
+  copy_to_clipboard(get_path(":t"))
 end, { nargs = 0, force = true, desc = "Copy just the file name to the clipboard" })
 
 vim.cmd("command! CopyFilePath CopyRelativeFilePath")
