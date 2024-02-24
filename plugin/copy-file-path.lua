89a49af8fc13bb4172070d7eb159aca6661e7710
mogulla3 <hs.iarn3@gmail.com>
2024-02-24 12:25:03 +0900

    Fix message format

--- plugin/copy-file-path.lua
+++ plugin/copy-file-path.lua
@@ -1,18 +1,18 @@
-local message_prefix = "[copy-file-path.nvim] "
+local function copy_to_clipboard(path)
+  vim.fn.setreg("+", path)
+  vim.api.nvim_echo({ { "Copied: " .. path } }, false, {})
+end
 
 vim.api.nvim_create_user_command("CopyRelativeFilePath", function()
-  vim.fn.setreg("+", vim.fn.expand("%"))
-  vim.api.nvim_echo({ { message_prefix .. "Copied relative file path." } }, false, {})
+  copy_to_clipboard(vim.fn.expand("%"))
 end, { nargs = 0, force = true, desc = "Copy relative file path to the clipboard" })
 
 vim.api.nvim_create_user_command("CopyAbsoluteFilePath", function()
-  vim.fn.setreg("+", vim.fn.expand("%:p"))
-  vim.api.nvim_echo({ { message_prefix .. "Copied absolute file path." } }, false, {})
+  copy_to_clipboard(vim.fn.expand("%:p"))
 end, { nargs = 0, force = true, desc = "Copy absolute file path to the clipboard" })
 
 vim.api.nvim_create_user_command("CopyFileName", function()
-  vim.fn.setreg("+", vim.fn.expand("%:t"))
-  vim.api.nvim_echo({ { message_prefix .. "Copied filename." } }, false, {})
+  copy_to_clipboard(vim.fn.expand("%:t"))
 end, { nargs = 0, force = true, desc = "Copy just the file name to the clipboard" })
 
 vim.cmd("command! CopyFilePath CopyRelativeFilePath")
